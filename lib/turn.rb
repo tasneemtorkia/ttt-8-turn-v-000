01f0637244e3b9387667b27e864d4341049f575a
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-04 16:20:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ def turn(board)
   number = input_to_index(index) 
   if !valid_move?(board, number)
     turn(board)
-    else if valid_move?(board, number)
+  else if valid_move?(board, number)
       move(board, number)
   end 
 end 
