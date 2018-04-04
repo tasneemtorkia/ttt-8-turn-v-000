dfba7405c6effbab916c596614ad95800dc73f51
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-04 14:41:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,9 @@ def turn(board)
   string = gets.strip
   input_to_index(string)
   if valid_move? 
-    move 
+    move(board, index, value = "X")
+  else turn
+  end 
 end 
 
 def display_board(board)
