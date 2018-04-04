c234cf5619b606ce449b9847135494d3d9527146
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-04 14:12:58 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,7 +1,6 @@
 def display_board(board)
-  print board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
 
 def valid_move?(board, index)
   if index.between?(0, 8) && !position_taken?(board, index)  
