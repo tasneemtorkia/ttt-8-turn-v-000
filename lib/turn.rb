75f3a8ef6398a1a6fa7187f08104bdf6597d4695
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-04 14:12:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,26 @@ def display_board(board)
   print board
 end
 
-def valid_move?(index)
-  
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)  
+  true
+else 
+  false
+end
+end
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else if board[index] == " " || board[index] == ""
+    false
+  else
+    false 
+  end
+  end
+end
+
+def move(board, index)
 end
