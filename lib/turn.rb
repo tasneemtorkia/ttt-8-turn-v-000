06971bc8a8edf314ebf6b5cdcdc782fcaa2933e8
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-04 14:22:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,7 @@
+def turn(board)
+  puts "Please enter 1-9:"
+end 
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} ", "-----------", " #{board[3]} | #{board[4]} | #{board[5]} ", "-----------", " #{board[6]} | #{board[7]} | #{board[8]} "
 end
